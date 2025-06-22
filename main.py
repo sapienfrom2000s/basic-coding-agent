659024b18e3a79f9acafe9dd070987887c829932
thirtyone <queenisuseless@gmail.com>
2025-06-22 22:07:44 +0530

    checkpoint: Ch1 complete

--- main.py
+++ main.py
@@ -14,29 +14,29 @@ verbose_flag = None
 
 from google.genai import types
 
+try:
+  prompt = sys.argv[1]
+  verbose_flag = sys.argv[2]
+
+except IndexError:
+  pass
+
+if prompt is None:
+  sys.exit(1)
+
 messages = [
   types.Content(role="user", parts=[types.Part(text=prompt)]),
 ]
 
-try:
-  prompt = sys.argv[1]
-  # verbose_flag = sys.argv[2]
-
-except IndexError:
-  print("Arg is out of index")
-
-if prompt is None:
-  print("No prompt provided")
-  sys.exit(1)
 
 response = client.models.generate_content(
   model="gemini-2.0-flash-001",
-  contents=prompt
+  contents=messages
 )
 
 print(response.text)
 
-# if verbose_flag == "--verbose":
-#   print(f"User prompt: {prompt}")
-#   print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-#   print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if verbose_flag == "--verbose":
+  print(f"User prompt: {prompt}")
+  print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+  print(f"Response tokens: {response.usage_metadata.candidates_token_count}")

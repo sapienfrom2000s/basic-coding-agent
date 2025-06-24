519a9611b7fc1bb576e4f89be1f764d859cbdd1d
thirtyone <queenisuseless@gmail.com>
2025-06-24 20:03:47 +0530

    ch2 l3

--- tests.py
+++ tests.py
@@ -1,6 +1,5 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-get_files_info("calculator", ".")
-get_files_info("calculator", "pkg")
-get_files_info("calculator", "/bin")
-get_files_info("calculator", "../")
+print(get_file_content("calculator", "main.py"))
+print(get_file_content("calculator", "pkg/calculator.py"))
+print(get_file_content("calculator", "/bin/cat"))
